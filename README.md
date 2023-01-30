9c8ae7f2e23f51b5cbad4af3f45153e36fe2a5ba
zgfg <51943147+zgfg@users.noreply.github.com>
2023-01-30 15:14:10 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,75 +1,23 @@
-# ToyBox-Ext
+# Systemless Debloater
 
 ### Description
-Systemless Magisk module
-to install (additional) ToyBox applets, using **ARMv7 or higher** binaries from:
-https://github.com/landley/toybox
+Select and systemlessly debloat preinstalled system apps. Supporting up to System As Root (SAR), Dynamic partitions and Android 13. Module must be installed through Magisk app, not TWRP - please read:
 
-**Supported**: ARMv7 and higher 
+[OP instructions at XDA](https://forum.xda-developers.com/mi-9t/how-to/magisk-module-systemless-debloater-t4180083)
 
-##### TEST
+#### Source
 
-Ussually, **ToyBox** comes preinstalled to `/system/bin`, but with limited number of **applets**.
-The module installes additional `toybox-ext` binary, with the additional applets (**UNIX/Linux command-line utilities**).
+[My Repo:](https://github.com/zgfg/SystemlessDebloater)
 
-**Install** the module and **reboot**. Use **Terminal Emulator** - **Toybox applets** are for Terminal and **shell scripts**.
+![GitHub release (latest by date)](https://img.shields.io/github/v/release/zgfg/SystemlessDebloater?label=Release&style=plastic) ![GitHub Release Date](https://img.shields.io/github/release-date/zgfg/SystemlessDebloater?label=Release%20Date&style=plastic) 
+![GitHub Releases](https://img.shields.io/github/downloads/zgfg/SystemlessDebloater/latest/total?label=Downloads%20%28Latest%20Release%29&style=plastic)
+![GitHub All Releases](https://img.shields.io/github/downloads/zgfg/SystemlessDebloater/total?label=Total%20Downloads%20%28All%20Releases%29&style=plastic)
 
-Make sure that `/system/bin` and/or `/system/xbin` (the latter might not be available on some devices) are in the `PATH`.
-Check e.g. with (all commands are **case sensitive**):
+[Alt-Repo:](https://github.com/Magisk-Modules-Alt-Repo/SystemlessDebloater)
 
-```
-su
-echo $PATH
-```
+![GitHub release (latest by date)](https://img.shields.io/github/v/release/Magisk-Modules-Alt-Repo/SystemlessDebloater?label=Release&style=plastic) ![GitHub Release Date](https://img.shields.io/github/release-date/Magisk-Modules-Alt-Repo/SystemlessDebloater?label=Release%20Date&style=plastic) 
+![GitHub Releases](https://img.shields.io/github/downloads/Magisk-Modules-Alt-Repo/SystemlessDebloater/latest/total?label=Downloads%20%28Latest%20Release%29&style=plastic)
+![GitHub All Releases](https://img.shields.io/github/downloads/Magisk-Modules-Alt-Repo/SystemlessDebloater/total?label=Total%20Downloads%20%28All%20Releases%29&style=plastic)
 
-Check if `toybox-ext` was properly installed and is it installed to `/system/xbin` or `/system/bin`:
-
-```
-toybox-ext --help
-which toybox-ext
-```
-
-If the responded toybox-ext path was `/system/xbin` then check:
-
-```
-ls -l /system/xbin | grep toybox-ext
-```
-
-whereas if the toybox-ext path was `/system/bin` then use:
-
-```
-ls -l /system/bin | grep toybox-ext
-```
-
-The command will show all toybox-ext applets as installed (symlinked to the toybox-ext binary).
-Usually, `w` will be one of them, hence you can try:
-
-```
-w
-```
-
-**Note 1**: Some applets are common to **BusyBox** and **ToyBox**, e.g. `ascii`.
-
-Hence, if you have also the `busybox` installed, `toybox-ext` will symlink less number of (additional) applets.
-
-**Note 2**: Since ToyBox-Ext **v1.0.2**, the module also looks if there are not-symlinked applets available from the pre-installed toybox binary (ROMs usually come with the pre-installed ToyBox but with smaller or an older binary and with a less number of applets).
-
-For me, the additionally found applets from the pre-installed ToyBox were e.g. `getfattr` and `iotop`.
-
-
-#### Source 
-
-[My Repo:](https://github.com/zgfg/ToyBox-Ext)
-
-![GitHub release (latest by date)](https://img.shields.io/github/v/release/zgfg/ToyBox-Ext?label=Release&style=plastic) ![GitHub Release Date](https://img.shields.io/github/release-date/zgfg/ToyBox-Ext?label=Release%20Date&style=plastic) 
-![GitHub Releases](https://img.shields.io/github/downloads/zgfg/ToyBox-Ext/latest/total?label=Downloads%20%28Latest%20Release%29&style=plastic)
-![GitHub All Releases](https://img.shields.io/github/downloads/zgfg/ToyBox-Ext/total?label=Total%20Downloads%20%28All%20Releases%29&style=plastic)
-
-[Alt-Repo:](https://github.com/Magisk-Modules-Alt-Repo/ToyBox-Ext)
-
-![GitHub release (latest by date)](https://img.shields.io/github/v/release/Magisk-Modules-Alt-Repo/ToyBox-Ext?label=Release&style=plastic) ![GitHub Release Date](https://img.shields.io/github/release-date/Magisk-Modules-Alt-Repo/ToyBox-Ext?label=Release%20Date&style=plastic) 
-![GitHub Releases](https://img.shields.io/github/downloads/Magisk-Modules-Alt-Repo/ToyBox-Ext/latest/total?label=Downloads%20%28Latest%20Release%29&style=plastic)
-![GitHub All Releases](https://img.shields.io/github/downloads/Magisk-Modules-Alt-Repo/ToyBox-Ext/total?label=Total%20Downloads%20%28All%20Releases%29&style=plastic)
-
-#### Copyright (c) zgfg @ xda, 2022-
+#### Copyright (c) zgfg @ xda, 2020-
 
--- customize.sh
+++ customize.sh
@@ -1,56 +1,475 @@
 #!/system/bin/sh
 
-# Magisk Module: ToyBox-Ext v1.0.4
-# Copyright (c) zgfg @ xda, 2022-
-# GitHub source: https://github.com/zgfg/ToyBox-Ext
+# Magisk Module: Systemless Debloater v1.5.0
+# Copyright (c) zgfg @ xda, 2020-
+# XDA thread: https://forum.xda-developers.com/mi-9t/how-to/magisk-module-systemless-debloater-t4180083
+# GitHub source: https://github.com/zgfg/SystemlessDebloater
 
 if [ -z $BOOTMODE ] ||  [ "$BOOTMODE" != "true" ] 
 then
 	abort "ERROR: Install from Magisk app, not from TWRP!"
 fi
 
-# Module's own path (local path)
-cd $MODPATH
 
-# toybox ARMv7 and higher binaries
-TBBINList="
-toybox-aarch64
-toybox-armv7m
-toybox-armv7l
-"
+# Magisk Module Installer variable
+REPLACE=""
 
-# Find the applicable binary
-TBFound=""
-for TBBIN in $TBBINList
+# Module's folder
+LogFolder=/storage/emulated/0/Download
+
+# Alternative path to Internal memory
+# LogFolder=/sdcard/Download
+
+# Module's version
+MyVersion=v1.5.0
+
+
+# Log file
+LogFile=$LogFolder/SystemlessDebloater.log
+PrintLine="Magisk Module: Systemless Debloater $MyVersion"
+echo "$PrintLine log file." > $LogFile
+echo 'Copyright (c) zgfg @ xda, 2020-' >> $LogFile
+echo "Installation time: $(date +%c)" >> $LogFile
+echo '' >> $LogFile
+
+# Log system info
+Prop=$(getprop ro.build.version.release)
+PrintLine='Android '$Prop
+Prop=$(getprop ro.build.system_root_image)
+if [ ! -z "$Prop" ] && [ "$Prop" ]
+then
+	PrintLine=$PrintLine' SAR'
+fi
+Prop=$(getprop ro.build.ab_update)
+if [ ! -z "$Prop" ] && [ "$Prop" ]
+then
+	PrintLine=$PrintLine' A/B'
+fi
+Prop=$(getprop ro.boot.slot_suffix)
+if [ ! -z "$Prop" ] && [ "$Prop" ]
+then
+	PrintLine=$PrintLine" ($Prop)"
+fi
+echo "$PrintLine"
+echo "$PrintLine" >> $LogFile
+PrintLine=$(magisk -c)
+echo "$PrintLine"
+echo "$PrintLine" >> $LogFile
+echo '' >> $LogFile
+
+
+# Default SAR mount-points (SAR partitions to search for debloating)
+#SarMountPointList="/system product /vendor /system_ext /india /my_bigball"  #toDo
+SarMountPointList=""
+
+# Invalid paths for (systemless) debloating
+#InvalidMountPointList="/data /apex /framework"  #toDo
+InvalidMountPointList="/data /framework"
+
+# Default/empty list of app names for debloating and debloated app names 
+DebloatList=""
+DebloatedList=""
+
+# Verbose logging 
+VerboseLog="true"
+
+# Searching for possible several instances of Stock apps for debloating  
+MultiDebloat="true"
+
+
+# Simple example for DebloatList var for the input file SystemlessDebloaterList.sh:
+#DebloatList="EasterEgg CatchLog Traceur wps_lite"
+
+# Input file with a list of app names for debloating
+DebloatListFile=$LogFolder/SystemlessDebloaterList.sh
+PrintLine="Input debloat list file: $DebloatListFile"												 					  
+echo "$PrintLine"
+echo "$PrintLine" >> $LogFile
+echo '' >> $LogFile
+
+# Check if the input list file exists
+if [ -f $DebloatListFile ]
+then
+	# Source the input file
+	. $DebloatListFile
+else
+	# Log error
+	PrintLine='Input file not found, creating a template file!'																					   
+	echo "$PrintLine"
+	echo "$PrintLine" >> $LogFile
+	echo "# Input debloat list $DebloatListFile for Magisk Module Systemless Debloater $MyVersion" > $DebloatListFile
+	echo '# Before debloating the apps, from Settings/Applications, Uninstall (updates) and Clear Data for them!' >> $DebloatListFile
+	echo "# Systemless Debloater log: $LogFile" >> $DebloatListFile
+	echo '# Copyright (c) zgfg @ xda, 2020-' >> $DebloatListFile
+	echo ' ' >> $DebloatListFile
+	echo '# Define a list of Stock apps for debloating:' >> $DebloatListFile
+	echo 'DebloatList=""' >> $DebloatListFile
+	echo ' ' >> $DebloatListFile
+	echo '# MIUI Example (commented out):' >> $DebloatListFile	
+	echo '# DebloatList="AnalyticsCore AntHalService BasicDreams ' >> $DebloatListFile 
+	echo '# BookmarkProvider CatchLog Chrome CneApp EasterEgg ' >> $DebloatListFile
+	echo '# facebook-appmanager facebook-installer facebook-services ' >> $DebloatListFile
+	echo '# FileExplorer_old GlobalFashiongallery GlobalMinusScreen ' >> $DebloatListFile 
+	echo '# Gmail2 GoogleFeedback GooglePartnerSetup HybridAccessory ' >> $DebloatListFile 
+	echo '# HybridPlatform IdMipay InMipay Joyose MiBrowserGlobal ' >> $DebloatListFile
+	echo '# MiBrowserGlobalVendor MiCreditInStub MiDrop ' >> $DebloatListFile
+	echo '# MiLinkService2 MiPicks MiPlayClient MiRcs MiRecycle ' >> $DebloatListFile
+	echo '# MiService MiuiBrowserGlobal MiuiBugReport MiuiDaemon ' >> $DebloatListFile
+	echo '# MSA-Global Netflix_activation PartnerBookmarksProvider ' >> $DebloatListFile
+	echo '# PaymentService PhotoTable Stk TouchAssistant Traceur ' >> $DebloatListFile
+	echo '# Turbo uceShimService Velvet VsimCore wps_lite YellowPage ' >> $DebloatListFile
+	echo '# Zman"' >> $DebloatListFile
+fi
+
+echo "Verbose logging: $VerboseLog" >> $LogFile
+echo "Multiple search/debloat: $MultiDebloat" >> $LogFile
+echo '' >> $LogFile
+
+
+# List Stock packages
+Packages=$(pm list packages -f | sed 's!^package:!!g')
+
+
+# Log input SarMountPointList
+if [ ! -z "$VerboseLog" ] && [ "$VerboseLog" = "true" ]
+then
+	echo 'Input SarMountPointList="'"$SarMountPointList"'"' >> $LogFile
+	echo '' >> $LogFile
+fi
+
+# Log InvalidMountPointList
+echo 'InvalidMountPointList="'"$InvalidMountPointList"'"' >> $LogFile
+echo '' >> $LogFile
+
+# Add /system to SarMountPointList
+NewList=$SarMountPointList
+SarMountPointList="/system"$'\n'
+for Path in $NewList
 do
-  if [ -z $TBFound ]
-  then
-    chmod 755 $TBBIN
-
-    # Test if binary executes 
-    Applets=$(./$TBBIN)
-
-    if [ ! -z "$Applets" ]
-    then
-      # Suitable binary found
-      echo "Installing $TBBIN binary and applets"
-      TBFound=true
-      mv $TBBIN toybox-ext
-      continue
-    fi
-  fi
-
-  # Delete binary (already found or doesn't execute)
-  rm -f $TBBIN
+	# Append to SarMountPointList
+	SarMountPointList="$SarMountPointList$Path"$'\n'
 done
 
-# Applicable binary not found
-if [ -z $TBFound ]
+# Search through packages to add potential mount points
+for PackageInfo in $Packages
+do
+	# Extract potential mount point path from PackageInfo
+	Path=$(echo "$PackageInfo" | cut -d '/' -f 2)
+
+	# Append to SarMountPointList
+	SarMountPointList="$SarMountPointList/$Path"$'\n'
+done
+
+# Sort SarMountPointList to remove duplicates
+NewList=$(echo "$SarMountPointList" | sort -bu )
+
+# Exclude not valid paths from SarMountPointList
+SarMountPointList=""
+for Path in $NewList
+do
+	# Skip not valid paths
+	for InvalidPath in $InvalidMountPointList
+	do
+		if [ "$Path" = "$InvalidPath" ]
+		then
+			Path=""
+			break
+		fi
+	done
+
+	# Append to SarMountPointList
+	if [ ! -z "$Path" ]
+	then
+		SarMountPointList="$SarMountPointList"$'\n'"$Path"
+	fi	
+done
+
+# Log final SarMountPointList 
+echo 'Final SarMountPointList="'"$SarMountPointList"'"' >> $LogFile
+echo '' >> $LogFile
+
+
+# Log input DebloatList 
+echo 'Input DebloatList="'"$DebloatList"'"' >> $LogFile
+echo '' >> $LogFile
+
+# Sort DebloatList
+NewList=$DebloatList
+DebloatList=""
+for AppName in $NewList
+do
+	# Append to DebloatList
+	DebloatList="$DebloatList$AppName"$'\n'
+done
+DebloatList=$(echo "$DebloatList" | sort -bu )
+
+# Log final DebloatList 
+if [ ! -z "$VerboseLog" ] && [ "$VerboseLog" = "true" ]
 then
-  echo
-  echo ERROR: Platform not supported!
-  echo
-  getprop | grep 'cpu\.abi'
-  echo
-  exit -1
+	echo 'Final DebloatList="'"$DebloatList"'"' >> $LogFile
+	echo '' >> $LogFile
 fi
+
+
+# List Stock packages
+PackageInfoList=""
+for PackageInfo in $Packages
+do
+	# Include only applications from SAR mount points
+	for SarMountPoint in $SarMountPointList
+	do		
+		if [ -z $(echo "$PackageInfo" | grep '^$SarMountPoint/') ]
+		then
+			PrepPackageInfo=$PackageInfo
+
+			# Append to the PackageInfoList
+			PackageInfoList="$PackageInfoList$PrepPackageInfo"$'\n'
+			
+			break
+		fi
+	done
+done
+
+# Sort PackageInfoList
+PackageInfoList=$(echo "$PackageInfoList" | sort -bu )
+
+
+#Search for Stock apps
+StockAppList=""
+for SarMountPoint in $SarMountPointList
+do
+	NewList=$(find "$SarMountPoint/" -type f -name "*.apk" 2> /dev/null)
+
+	if [ ! -z "$NewList" ]
+	then
+		StockAppList="$StockAppList$NewList"$'\n'
+	fi
+done
+
+
+#Search for previously debloated Stock apps
+ReplacedAppList=""
+for SarMountPoint in $SarMountPointList 
+do
+	NewList=$(find "$SarMountPoint/" -type f -name ".replace" 2> /dev/null)
+
+	if [ ! -z "$NewList" ]
+	then
+		ReplacedAppList="$ReplacedAppList$NewList"$'\n'
+	fi
+done
+
+# Sort ReplacedAppList
+#ReplacedAppList=$(echo "$ReplacedAppList" | sort -bu )
+
+# Log ReplacedAppList
+echo "Previously debloated Stock apps:"$'\n'"$ReplacedAppList" >> $LogFile
+
+
+# Prepare service.sh file to debloat Stock but not System apps
+ServiceScript="$MODPATH/service.sh"
+echo "ServiceScript: $ServiceScript" >> $LogFile
+echo '' >> $LogFile
+
+echo '#!/system/bin/sh' > $ServiceScript
+echo '' >> $ServiceScript
+
+echo "# Magisk Module: Systemless Debloater $MyVersion" >> $ServiceScript
+echo '# Copyright (c) zgfg @ xda, 2020-' >> $ServiceScript
+echo "# Installation time: $(date +%c)" >> $ServiceScript
+echo '' >> $ServiceScript
+
+# Log file for service.sh
+echo 'ServiceLogFolder=/data/local/tmp' >> $ServiceScript
+echo 'ServiceLogFile=$ServiceLogFolder/SystemlessDebloater-service.log' >> $ServiceScript
+echo '' >> $ServiceScript
+
+if [ ! -z "$VerboseLog" ] && [ "$VerboseLog" = "true" ]
+then
+	echo 'echo "Execution time: $(date +%c)" > $ServiceLogFile' >> $ServiceScript
+	echo 'echo "" >> $ServiceLogFile' >> $ServiceScript
+else
+	echo 'rm $ServiceLogFile' >> $ServiceScript
+fi
+echo '' >> $ServiceScript
+
+# Module's own folder
+MODDIR=$(echo "$MODPATH" | sed "s!/modules_update/!/modules/!")
+echo "MODDIR=$MODDIR" >> $ServiceScript
+								   
+
+if [ ! -z "$VerboseLog" ] && [ "$VerboseLog" = "true" ]
+then	
+	echo 'echo "MODDIR: $MODDIR" >> $ServiceLogFile' >> $ServiceScript								
+	echo 'echo "" >> $ServiceLogFile' >> $ServiceScript								
+	echo '' >> $ServiceScript
+fi
+
+# Dummy apk used for debloating
+echo 'DummyApk=$MODDIR/dummy.apk' >> $ServiceScript							   
+echo 'touch $DummyApk' >> $ServiceScript							   
+echo '' >> $ServiceScript
+
+if [ ! -z "$VerboseLog" ] && [ "$VerboseLog" = "true" ]
+then
+	echo 'echo "DummyApk: $DummyApk" >> $ServiceLogFile' >> $ServiceScript							
+	echo 'echo "" >> $ServiceLogFile' >> $ServiceScript							
+	echo '' >> $ServiceScript
+fi
+
+# Mount and bind for debloating
+echo 'MountBind="mount -o bind"' >> $ServiceScript
+echo '' >> $ServiceScript
+
+# List of apps to debloat by mounting
+MountList=""
+
+
+# Iterate through apps for debloating
+echo 'Debloating:' >> $LogFile
+for AppName in $DebloatList
+do
+	AppFound=""
+
+	#Search through previously debloated Stock apps	
+	SearchName=/"$AppName"/.replace
+	SearchList=$(echo "$ReplacedAppList" | grep "$SearchName$")
+	for FilePath in $SearchList
+	do
+		# Break if app already found
+		if [ -z "$MultiDebloat" ] && [ "$MultiDebloat" = "true" ]
+		then
+			if [ ! -z "$AppFound" ] 
+			then
+				break					
+			fi
+		fi
+	
+		# Remove /filename from the end of the path
+		FileName=${FilePath##*/}
+		FolderPath=$(echo "$FilePath" | sed "s,/$FileName$,,")
+					
+		if [ ! -z "FolderPath" ]
+		then
+			AppFound="true"
+
+			# Log the full path
+			echo "found: $FilePath" >> $LogFile
+				
+			if [ -z $(echo "$FolderPath" | grep '^/system/') ]
+			then
+				# Append to MountList with appended AppName
+				MountList="$MountList$FolderPath/$AppName.apk"$'\n'			
+			else
+				# Append to REPLACE list
+				REPLACE="$REPLACE$FolderPath"$'\n'			
+			fi
+			
+			# Append to DebloatedList
+			DebloatedList="$DebloatedList$AppName"$'\n'
+		fi
+	done
+
+	#Search through Stock apps	
+	SearchName=/"$AppName".apk
+	SearchList=$(echo "$StockAppList" | grep "$SearchName$")
+	for FilePath in $SearchList
+	do
+		if [ -z "$MultiDebloat" ] && [ "$MultiDebloatt" = "true" ]
+		then
+			if [ ! -z "$AppFound" ] 
+			then
+				break					
+			fi
+		fi
+
+		# Find the corresponding package
+		PackageInfo=$(echo "$PackageInfoList" | grep "$FilePath")
+		PackageName=""
+
+		# Extract package name
+		if [ ! -z "$PackageInfo" ]
+		then
+			PackageName=$(echo "$PackageInfo" | sed "s!^$FilePath=!!")
+			PackageName="($PackageName) "
+		fi
+			
+		# Remove /filename from the end of the path
+		FileName=${FilePath##*/}
+		FolderPath=$(echo "$FilePath" | sed "s,/$FileName$,,")
+					
+		if [ ! -z "FolderPath" ]
+		then
+			AppFound="true"
+
+			# Log the full path and package name
+			echo "found: $FilePath $PackageName" >> $LogFile
+
+			if [ -z $(echo "$FolderPath" | grep '^/system/') ]
+			then
+				# Append to MountList
+				MountList="$MountList$FilePath"$'\n'			
+			else
+				# Append to REPLACE list
+				REPLACE="$REPLACE$FolderPath"$'\n'			
+			fi
+			
+			# Append to DebloatedList
+			DebloatedList="$DebloatedList$AppName"$'\n'
+		fi
+	done
+	
+	if [ -z "$AppFound" ]
+	then
+		# Log app name if not found
+		PrintLine="$AppName --- app not found!"
+		echo "$PrintLine"
+		echo "$PrintLine" >> $LogFile
+	fi
+done
+echo '' >> $LogFile
+
+if [ -z "$REPLACE" ]
+then
+	PrintLine="No app for debloating found!"
+	echo "$PrintLine"
+	echo "$PrintLine" >> $LogFile
+	PrintLine='Before debloating the apps, from Settings/Applications, Uninstall (updates) and Clear Data for them!'
+	echo "$PrintLine"
+	echo "$PrintLine" >> $LogFile
+	echo '' >> $LogFile
+fi
+
+# Sort and log DebloatedList
+DebloatedList=$(echo "$DebloatedList" | sort -bu )
+echo 'DebloatedList="'"$DebloatedList"$'\n"' >> $LogFile
+echo '' >> $LogFile
+
+# Sort and log REPLACE list
+REPLACE=$(echo "$REPLACE" | sort -bu )
+echo 'REPLACE="'"$REPLACE"$'\n"' >> $LogFile
+echo '' >> $LogFile
+
+# Sort and log MountList 
+MountList=$(echo "$MountList" | sort -bu )
+echo 'MountList="'"$MountList"$'\n"' >> $LogFile
+echo '' >> $LogFile
+
+# Debloat by mounting in servise.sh
+for MountApk in $MountList
+do
+	PrintLine='$MountBind $DummyApk '"$MountApk"
+	echo "$PrintLine" >> $ServiceScript
+done
+
+
+# Log Stock apps and packages
+if [ ! -z "$VerboseLog" ] && [ "$VerboseLog" = "true" ]
+then
+	echo "Stock apps:"$'\n'"$StockAppList" >> $LogFile
+	echo "Stock packages: $PackageInfoList" >> $LogFile
+fi
+
+
+# Note for the log file
+echo "Systemless Debloater log: $LogFile"
